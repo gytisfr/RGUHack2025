54fb9d732ef582282fd5c69aed5735257a7a150a
Copciop23 <138177233+Copciop23@users.noreply.github.com>
2025-02-22 15:36:17 +0000

    Encounter

--- Backend/structs.py
+++ Backend/structs.py
@@ -1,6 +1,10 @@
 class encounter:
     def __init__(self, data : list):
         self.uid = data[0]
-        self.location = data[1]
-        self.time = data[2]
-        self.extra = data[3]
+        self.animal = data[1] 
+        self.animaltype = data[2]
+        self.userid = data[3]
+        self.location = data[4]
+        self.time = data[5]
+        self.extra = data[6]
+        self.verified =  data[7]
