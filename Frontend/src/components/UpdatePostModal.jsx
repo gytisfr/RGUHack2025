14eacd8440286d4c79f92687ec2c05b259da6c18
bfoub <143612934+bfoub@users.noreply.github.com>
2025-02-23 10:18:27 +0000

    Update UpdatePostModal.jsx

--- Frontend/src/components/UpdatePostModal.jsx
+++ Frontend/src/components/UpdatePostModal.jsx
@@ -3,39 +3,29 @@ import axios from "axios";
 import Cookies from "js-cookie";
 
 export const UpdatePostModal = (props) => {
-    const [animal, setAnimal] = useState("");
-    const [type, setType] = useState("");
-    const [position, setPosition] = useState({ lat: null, lng: null });
+    const [attribute, setAttribute] = useState("");
+    const [newValue, setNewValue] = useState("");
     const token = Cookies.get("token");
-
-    useEffect(() => {
-        if (navigator.geolocation) {
-            navigator.geolocation.getCurrentPosition((pos) => {
-                setPosition({
-                    lat: pos.coords.latitude,
-                    lng: pos.coords.longitude
-                });
-            });
-        }
-    }, []);
 
     const handleSubmit = async (e) => {
         e.preventDefault();
         try {
-            console.log(token)
-            const response = await axios.post(`https://api.rguhacknature.co.uk/encounter?animal=${animal}&animalType=${type}&longitude=${position.lng}&latitude=${position.lat}&token=${token}`, {
-                animal,
-                type,
-                position
+            console.log(props.sighting.uniqueID)
+            const response = await axios.patch(`https://api.rguhacknature.co.uk/encounter?uniqueID=${props.sighting.uniqueID}&what=${attribute}&to=${newValue}&token=${token}`, {
+                uniqueID: props.sighting.uniqueID,
+                attribute: attribute,
+                data: newValue,
+                token: token
             }, {
                 headers: {
                     Authorization: `Bearer ${token}`
                 }
             });
-            console.log("Sighting submitted:", response.data);
+            console.log("Sighting updated:", response.data);
+            props.updateMockData(prevData => prevData.map(item => item.uniqueID === props.sighting.uniqueID ? { ...item, [attribute]: newValue } : item));
             props.setModalOpen(false);
         } catch (error) {
-            console.error("Error submitting sighting:", error);
+            console.error("Error updating sighting:", error);
         }
     };
 
@@ -43,41 +33,32 @@ export const UpdatePostModal = (props) => {
         <div className="modal-background">
             <form className="modal-form" onSubmit={handleSubmit}>
                 <header className="modal-header">
-                    <h1>Enter Sighting</h1>
+                    <h1>Update Sighting</h1>
                     <button type="button" onClick={() => { props.setModalOpen(false) }}>
                         <img src="../src/assets/X.png" alt="Close" />
                     </button>
                 </header>
                 <div className="modal-body">
-
-                    <p>Animal</p>
-                        <input
-                            type="text"
-                            value={animal}
-                            onChange={(e) => setAnimal(e.target.value)}
-                            placeholder="Enter animal name"
-                            required
-                        />
-
-                    <p>Type</p>
-                        <select value={type} onChange={(e) => setType(e.target.value)} required>
-                            <option value="">Select Type</option>
-                            <option value="dog">Dog</option>
-                            <option value="cat">Cat</option>
-                            <option value="bird">Bird</option>
-                            <option value="mammal">Mammal</option>
-                            <option value="reptile">Reptile</option>
-                            <option value="arachnid">Arachnid</option>
-                            <option value="amphibian">Amphibian</option>
-                            <option value="mollusk">Mollusk</option>
-                            <option value="echinoderm">Echinoderm</option>
-                            <option value="insect">Insect</option>
-                            <option value="cnidarian">Cnidarian</option>
-                        </select>
-
+                    <p>Attribute</p>
+                    <select value={attribute} onChange={(e) => setAttribute(e.target.value)} required>
+                        <option value="">Select Attribute</option>
+                        <option value="animal">Animal</option>
+                        <option value="type">Type</option>
+                        <option value="lng">Longitude</option>
+                        <option value="lat">Latitude</option>
+                        <option value="time">Time</option>
+                    </select>
+                    <p>New Value</p>
+                    <input
+                        type="text"
+                        value={newValue}
+                        onChange={(e) => setNewValue(e.target.value)}
+                        placeholder="Enter new value"
+                        required
+                    />
                 </div>
                 <footer className="modal-footer">
-                    <button type="submit" style={{color: "white", fontSize: "17px", fontWeight: 600, float: "right", backgroundColor: "green", marginTop: "1em"}}>Submit</button>
+                    <button type="submit" style={{ color: "white", fontSize: "17px", fontWeight: 600, float: "right", backgroundColor: "green", marginTop: "1em" }}>Submit</button>
                 </footer>
             </form>
         </div>
